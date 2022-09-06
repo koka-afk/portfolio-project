f09d5c45b5b953a6774706369138a9e6a65e858d
yosef samy <yosefsamykoka@gmail.com>
2022-09-06 14:51:52 +0200

    Migrating to postgres and making blogs model

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "portfoliodb",
+        "USER": "postgres",
+        "PASSWORD": "open4me",
+        "HOST": "localhost",
+        "PORT": '5432',
     }
 }
 
@@ -112,7 +117,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
