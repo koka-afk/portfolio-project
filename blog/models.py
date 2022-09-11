d0301f984023471be4be7efe91d77bf1c41b1b80
yosef samy <yosefsamykoka@gmail.com>
2022-09-11 18:48:12 +0200

    Completed the blogs section, polished the website and it is ready to go live

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,15 @@ class blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) > 1000:
+            return '"' + self.body[:100] + '..."'
+        else:
+            return '"' + self.body + '"'
+
+    def pub_date_format(self):  # might not use but it's good to have options
+        return self.pub_date.strftime('%b %e %Y')
